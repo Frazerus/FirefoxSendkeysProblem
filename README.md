41b43bf494251416ee8367e26f06adb8d926a343
Lukas Hartinger <lukas.hartinger@rubicon.eu>
2023-03-24 11:46:13 +0100

    Test-project setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# FirefoxSendkeysProblem
+# Firefox SendKeys Problem
+Since firefox v99, the `SendKeys` operation by selenium does not input values when being sent with a previous delete operation.
+
+This is a small project to showcase this problem.
+
+Tested with Firefox v112.0b5
---
+++ TestGeckoWebdriverSendKeys/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+var mainDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+
+var firefoxOptions = new FirefoxOptions();
+firefoxOptions.LogLevel = FirefoxDriverLogLevel.Trace;
+var driver = new FirefoxDriver(mainDirectory + @"\driver\", firefoxOptions);
+
+var url = Path.Combine(mainDirectory, "index.html");
+driver.Navigate().GoToUrl(url);
+
+var element = driver.FindElement(By.Id("textInput"));
+
+element.SendKeys("inputWithoutDelete");
+
+Debug.Assert(element.GetAttribute("value") == "inputWithoutDelete", "element.Value == 'inputWithoutDelete'");
+
+var deleteInput = Keys.Control + 'a' + Keys.Control + Keys.Delete;
+var valueInput = "myInput";
+
+//This SHOULD already input the correct value, right?
+element.SendKeys(deleteInput + valueInput);
+
+Debug.Assert(element.GetAttribute("value") != valueInput, "element.Value != inputValue");
+Debug.Assert(element.GetAttribute("value") == string.Empty, "element.Value == string.Empty");
+
+//This actually does what is required.
+element.SendKeys(deleteInput);
+element.SendKeys(valueInput);
+
+Debug.Assert(element.GetAttribute("value") == valueInput, "element.Value == inputValue");
